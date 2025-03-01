eca204bd66ee7a911d2fb73404c73da917413c25
SinethembaDlova <dsinethemba@gmail.com>
2025-03-01 02:32:49 +0200

    test: add home page redirect verification to movies route
    - Add Cypress e2e test verifying root URL redirects to /movies
    - Add support file setup to resolve Cypress configuration warning

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,6 @@
+describe('Home Page Redirect Test', () => {
+    it('should automatically redirect to /movies when visiting the home page', () => {
+      cy.visit('/');
+      cy.location('pathname').should('eq', '/movies');
+    });
+  });
